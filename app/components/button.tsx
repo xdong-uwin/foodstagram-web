2b0700399bcc3308a2bda13342490a185803af5a
Xiangrui Dong <dong88@uwindsor.ca>
2024-11-11 19:00:29 -0500

    adjust the layout of button like

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
             },
             size: {
                 default: "h-10 px-6 py-2",
-                sm: "h-9 rounded-md px-3",
+                sm: "h-9 rounded-md px-1",
             },
         },
         defaultVariants: {
