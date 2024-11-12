921c495212b3788c39b3d9e3244259b239d5c08b
Xiangrui Dong <dong88@uwindsor.ca>
2024-11-11 21:35:50 -0500

    fetch recipes through http request

--- app/components/recipes.tsx
+++ app/components/recipes.tsx
@@ -2,30 +2,45 @@ import {Button} from "@/components/button";
 import { Heart } from "lucide-react"
 import {Recipe, RecipeContent} from "@/components/recipe";
 import Image from "next/image"
+import useSWR from "swr";
+
+type Recipe = {
+    imageUrl: string
+    title: string
+    description: string
+    authorId: string
+}
+
+const fetcher = (...args: [RequestInfo, RequestInit?]): Promise<Recipe[]> =>
+    fetch(...args).then((res) => res.json());
 
 export default function Recipes() {
-    const posts = Array(10).fill({
-        image: "/placeholder.jpeg?height=400&width=400",
-        title: "Welcome to foodstagram! Find your specialized food.",
-        poster: "Poster"
-    })
+    const { data } = useSWR('http://localhost:8080/v1/recipes', fetcher);
+
+    const recipes = data ? data.map(recipe => ({
+        "image": recipe.imageUrl,
+        "title": recipe.title,
+        "description": recipe.description,
+        "authorId": recipe.authorId,
+    })) : [];
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
-            {posts.map((post, index) => (
+            {recipes.map((recipe, index) => (
                 <Recipe key={index}>
                     <RecipeContent className="p-3">
-                        <div className="aspect-square relative mb-5">
+                        <div className="aspect-square relative mb-3">
                             <Image
-                                src={post.image}
+                                src={recipe.image}
                                 alt="Food post"
                                 fill
                                 className="object-cover rounded-lg"
                             />
                         </div>
-                        <p className="text-sm mb-2">{post.title}</p>
+                        <p className="text-lg mb-2 font-extrabold">{recipe.title}</p>
+                        <p className="text-sm mb-2 line-clamp-2">{recipe.description}</p>
                         <div className="flex items-center justify-between">
-                            <span className="text-sm text-gray-500">{post.poster}</span>
+                            <span className="text-sm text-gray-500">{recipe.authorId}</span>
                             <Button
                                 variant="ghost"
                                 size="sm"
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
 };
+
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.dropbox.com'
+            },
+        ],
+    },
+}
 
 export default nextConfig;
