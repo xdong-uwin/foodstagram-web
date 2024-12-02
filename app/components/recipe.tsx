8ede2c2c56df94921f2d99e5d125c71404e4f61a
Xiangrui Dong <dong88@uwindsor.ca>
2024-12-01 21:04:45 -0500

    refactor recipe component

--- app/components/recipe.tsx
+++ app/components/recipe.tsx
@@ -1,64 +1,22 @@
 import * as React from "react"
-import {merge} from "@/lib/utils"
+import type {Recipe} from "@/types/recipe";
+import RecipeContent from "@/components/recipe-content";
+import {RecipeDetailModal} from "@/components/modal/recipe-detail-modal";
 
-const Recipe = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-    <div
-        ref={ref}
-        className={merge("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
-        {...props}
-    />
-))
-Recipe.displayName = "Recipe"
 
-const RecipeHeader = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-    <div
-        ref={ref}
-        className={merge("flex flex-col space-y-1.5 p-6", className)}
-        {...props}
-    />
-))
-RecipeHeader.displayName = "RecipeHeader"
+export default function Recipe(
+    props: { recipe: Recipe }
+) {
+    const [isDetailModalOpen, setIsDetailModalOpen] = React.useState(false)
 
-const RecipeTitle = React.forwardRef<
-    HTMLParagraphElement,
-    React.HTMLAttributes<HTMLHeadingElement>
->(({className, ...props}, ref) => (
-    <h3
-        ref={ref}
-        className={merge("text-2xl font-semibold leading-none tracking-tight", className)}
-        {...props}
-    />
-))
-RecipeTitle.displayName = "RecipeTitle"
-
-const RecipeDescription = React.forwardRef<
-    HTMLParagraphElement,
-    React.HTMLAttributes<HTMLParagraphElement>
->(({className, ...props}, ref) => (
-    <p
-        ref={ref}
-        className={merge("text-sm text-muted-foreground", className)}
-        {...props}
-    />
-))
-RecipeDescription.displayName = "RecipeDescription"
-
-const RecipeFooter = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-    <div
-        ref={ref}
-        className={merge("flex items-center p-6 pt-0", className)}
-        {...props}
-    />
-))
-RecipeFooter.displayName = "RecipeFooter"
-
-export {Recipe, RecipeHeader, RecipeFooter, RecipeTitle, RecipeDescription}
+    return (
+        <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+            <RecipeContent recipe={props.recipe} setIsDetailModalOpen={setIsDetailModalOpen} />
+            <RecipeDetailModal
+                recipe={props.recipe}
+                open={isDetailModalOpen}
+                onOpenChange={setIsDetailModalOpen}
+            />
+        </div>
+    )
+}
--- app/components/recipes.tsx
+++ app/components/recipes.tsx
@@ -1,24 +1,14 @@
-import {Recipe} from "@/components/recipe";
+import Recipe from "@/components/recipe";
 import React from "react";
-import {RecipeDetailModal} from "@/components/modal/recipe-detail-modal";
 import {Recipe as RecipeDto} from "@/types/recipe";
-import RecipeContent from "@/components/recipe-content";
 
 export default function Recipes(props: { recipes: RecipeDto[] }) {
-    const [isDetailModalOpen, setIsDetailModalOpen] = React.useState(false)
+
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
             {props.recipes.map((recipe, index) => (
-                <Recipe key={index}>
-                    <RecipeContent recipe={recipe} setIsDetailModalOpen={setIsDetailModalOpen} />
-                    <RecipeDetailModal
-                        recipe={recipe}
-                        open={isDetailModalOpen}
-                        onOpenChange={setIsDetailModalOpen}
-                    />
-                </Recipe>
-
+                <Recipe recipe={recipe} key={index} />
             ))}
         </div>
     )
